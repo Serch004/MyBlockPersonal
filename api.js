e4c0db79a106c1fcf4f533b23e9773d7b116b20e
serch004 <ing.sergio.pichardo.a@gmail.com>
2023-03-27 13:36:09 -0600

    Se agrego un apartado para comentarios en el HTML y un archivo json para jalar los comentarios como una API

---
+++ api.js
@@ -0,0 +1,51 @@
+[
+    {
+        "idComentario": "Comentario1",
+        "fecha": "22 de marzo",
+        "usuario": "rodo4",
+        "comentario": "que buen block mi inge sigue asi."
+    },
+    {
+        "idComentario": "Comentario2",
+        "fecha": "19 de marzo",
+        "usuario": "perez005",
+        "comentario": "El diseño de tu pag es muy bueno, el unico detalle es que no es responsive."
+    },
+    {
+        "idComentario": "Comentario3",
+        "fecha": "14 de marzo",
+        "usuario": "",
+        "comentario": "Buena presentacion, se ve muy bien tu block."
+    },
+    {
+        "idComentario": "Comentario4",
+        "fecha": "14 de marzo",
+        "usuario": "Nombre",
+        "comentario": "no me gusto ya que no es responsive y aparte las imagenes estan descuadradas "
+    },
+    {
+        "idComentario": "Comentario5",
+        "fecha": "09 de marzo",
+        "usuario": "Nombre",
+        "comentario": "Buen trabajo amigo."
+    },
+    {
+        "idComentario": "Comentario6",
+        "fecha": "27 de abril",
+        "usuario": "Nombre",
+        "comentario": "Para ser tu primera pagina se mira bastante bien"
+    },
+    {
+        "idComentario": "Comentario7",
+        "fecha": "13 de abril",
+        "usuario": "Nombre",
+        "comentario": "Programaste todo con java?"
+    },
+    {
+        "idComentario": "Comentario8",
+        "fecha": "25 de febrero",
+        "usuario": "Nombre",
+        "comentario": "Que buen trabajo hermano se ve bastante bien."
+    }
+    
+]
